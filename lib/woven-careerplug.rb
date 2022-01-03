1389a80747d4fde262abad3eb2cb423aa2743a28
Leah LaSalla <leah@opensorceress.net>
2022-01-03 13:59:30 -0600

    Housekeeping, comments. Each point where comments are places is a good candidate for refactoring / broken out into a method for better code organization, etc. when writing for tests etc. I don't have the output format so I did my best from memory. Also based on the examples provided and code setup it seems like daily calculations were expected support but from how I understood the task, calculating each day would be unnecessary

--- lib/woven-careerplug.rb
+++ lib/woven-careerplug.rb
@@ -41,7 +41,8 @@ date = '2019-01'
 # @return [Hash]
 def bill_for(month, active_subscription = nil, users = {})
 
-  date = "#{month.to_s}-01"
+  # build up the billing cycle
+  date = "#{month}-01"
   year = Date.parse(date.to_s).year
   month = Date.parse(date.to_s).month
 
@@ -53,56 +54,58 @@ def bill_for(month, active_subscription = nil, users = {})
   invoice_start_date = Date.new(year.to_i, month.to_i, Date.parse(first_day.to_s).day.to_i)
   invoice_end_date = Date.new(year.to_i, month.to_i, Date.parse(last_day.to_s).day.to_i)
 
-  active_users = :active_users
-  total_usage = :total_usage
-  billing_period = :billing_period
-
+  # set up the invoice object defaults
   invoice = {}
-  invoice[billing_period] = [invoice_start_date.to_s, invoice_end_date.to_s]
-  invoice[total_usage] = 0
-  invoice[active_users] = 0
+  invoice['billing_period'] = [invoice_start_date.to_s, invoice_end_date.to_s]
+  invoice['total_usage'] = 0
+  invoice['active_users'] = 0
+  invoice['deactivated_users'] = 0
 
+  # sanity check and return defaults
   return invoice if active_subscription.nil? || users.length.zero?
 
-  deactivated_users = 0
-
+  # pull values from the subscription object and set up this months daily bill rate
   monthly_rate = :monthly_price_in_dollars
   if !active_subscription.nil? && users.length.positive?
     daily_rate = active_subscription[monthly_rate].to_f / days_in_month.to_i
 
+    # iterate thru each employee object
     users.each do |employee|
 
-      invoice[active_users] += 1
+      # setup defaults and symbols; increment users
+      invoice['active_users'] += 1
       activated_on = :activated_on
       deactivated_on = :deactivated_on
       id = :id
 
+      # check if the user was activated after the start of the billing cycle
       start_date = invoice_start_date
       start_date = employee[activated_on] if employee[activated_on] > invoice_start_date
 
+      # check if the user was deactivated prior to the end of the billing cycle
       end_date = invoice_end_date
+      # determine user activity within the current billing cycle and test for deactivations 
       unless employee[deactivated_on].nil?
         if [Date.parse(employee[deactivated_on].to_s).year, Date.parse(employee[deactivated_on].to_s).month] ==
            [Date.parse(invoice_end_date.to_s).year, Date.parse(invoice_end_date.to_s).month] &&
            Date.parse(employee[deactivated_on].to_s).day < Date.parse(invoice_end_date.to_s).day
           end_date = employee[deactivated_on]
         end
-        deactivated_users += 1
+        # increment deactivations
+        invoice['deactivated_users'] += 1
       end
+      
+    # calculate the number of active days for this
+      active_days = end_date - start_date
+      employee_usage = daily_rate * active_days
 
-    active_days = end_date - start_date
-    employee_usage = daily_rate * active_days
-
-    employee_id = "employee_#{employee[id]}"
-    invoice[employee_id] = employee_usage.round(2)
-      total = invoice[total_usage].to_f + employee_usage.to_f
-      invoice[total_usage] = total.round(2)
+      # capture each user's line item usage for this billing cycle, caluclate compound total and round off floats
+      employee_id = "employee_#{employee[id]}"
+      invoice[employee_id] = employee_usage.round(2)
+      invoice['total_usage'] += employee_usage.to_f
+      invoice['total_usage'] = invoice['total_usage'].round(2)
     end
   end
-
-
-  invoice['active_users'] = active_users
-  invoice['users_deactivated'] = deactivated_users
   puts invoice
   invoice
 end
